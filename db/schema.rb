e0eb1f67b2225638ee28a86c63ae4bfc4247c07b
juan grover <jpgrover@gmail.com>
2021-09-02 17:28:18 -0400

    Right index wear

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,23 @@ ActiveRecord::Schema.define(version: 2021_09_01_132307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.text "nombre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "price"
+    t.integer "cantidad"
+    t.bigint "wear_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["wear_id"], name: "index_order_items_on_wear_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "monto"
@@ -43,5 +60,20 @@ ActiveRecord::Schema.define(version: 2021_09_01_132307) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wears", force: :cascade do |t|
+    t.string "talla"
+    t.string "color"
+    t.string "descripcion"
+    t.string "marca"
+    t.integer "valor"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_wears_on_category_id"
+  end
+
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "wears"
   add_foreign_key "orders", "users"
+  add_foreign_key "wears", "categories"
 end
