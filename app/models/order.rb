743976d45546683dafb2ce6470f2409c24265e85
V. Bruk <vbruk@hey.com>
2021-09-07 16:28:51 -0300

    Fix Order & OrderItem

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :wears, through: :OrderItem
-  has_many :OrderItem
+
+  has_many :order_items, dependent: :destroy
+  has_many :wears, through: :order_items
+
+
   validates :monto, presence: true
   validates :metodopago, presence: true
   validates :address, presence: true
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :wear
   belongs_to :order
+
   validates :monto, presence: true
   validates :cantidad, presence: true
 end
