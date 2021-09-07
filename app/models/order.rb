44d2cada022bac46a0cc3ff034560c67476ace1c
V. Bruk <vbruk@hey.com>
2021-09-07 16:34:39 -0300

    Resolver issue con montos y subotales de la orden

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,20 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :wears, through: :order_items
 
-
-  validates :monto, presence: true
   validates :metodopago, presence: true
   validates :address, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :country, presence: true
   validates :address, presence: true
+
+  after_create_commit :update_order_subtotal
+  after_update_commit :update_order_subtotal
+
+  private
+
+  def update_order_subtotal
+    monto_total = self.order_items.sum(:total_price)
+    update(mont: monto_total)
+  end
 end
