c11e0b9e4b29a54efc330bd9af21eda471b74ca2
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-09-21 22:47:33 -0300

    realizacion de seed comentado por ahora para revision

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@
 # Category.create(nombre: "Pantalones")
 # Category.create(nombre: "Remeras")
 
+
+#User.create (email: "lfuentes.entergrafic@gmail.com", password: "15051996", admin: true)
+#User.create (email:"jpgrover@mounkey.cl", password: "12345678", admin: true)
+#User.create (email: "entergr4fic@gmail.com" password: "12345678", admin: false)
+#User.create (email: "megalatinvoz@gmail.com", password: "12345678", admin: false)
+
+#wear = Wear.create([{marca: 'fiorucci', color: 'rosado', talla: '42', descripcion: 'chaqueta mezclilla color rosa', valor: '20.000', category_id: '2', user_id: '1'}, {marca: 'zara', color: 'floreado', talla: 's', descripcion: 'vestido corto genero liviano', valor: '15.000', category_id: '1', user_id: '1'}, {marca: 'levis', color: 'denim', talla: '44', descripcion: 'jeans short gastado', talla: '14', category_id: '1', user_id: '1'}, {marca: 'oneil', color: 'azul', talla: 'm', descripcion: 'traje de baño piezas sin uso', category_id: '2', valor: '3000', category_id: '1', user_id: '1'}, {marca: 'werever', color: 'café', talla: 'xl', descripcion: 'falda café con botones y etc ejemplo largo', valor: '3000', category_id: '1', user_id: '1'}, {marca: 'infanti', color: 'rosado', talla: '3', descripcion: 'chaleco forrado niña', valor: '3000', category_id: '3', user_id: '1'}, {marca: 'casa_blanca', color: 'blanco', talla: '3', descrpcion: 'vestido fiesta niña', valor: '10000', category_id: '3', user_id: '1'}, {marca: 'mary', color: 'rojo', talla: '43', descrpcion: 'vestido fiesta lola', valor: '1', category_id: '1', user_id: '6'}, {marca: 'caballito', color: 'rojo', talla: '2', descripcion: 'tenida', valor: '30', category_id: '5', user_id: '1'}])
+
+
+#Category.create(nombre: "Ropa_Mujer", photo: "picturesWomanbig.png")
+#Category.create(nombre: "Ropa_Kids", photo: "picturesKidssmall.png")
+#Category.create(nombre: "Ropa_Hombre", photo: "picturesManbig.png")
+#Category.create(nombre: "Ropa_Winter", photo: "picturesWintersmall.png")
+#Category.create(nombre: "Ofertas", photo: "picturesSalesmall.png")
+#Category.create(nombre: "Party", photo:"picturesPartysmall.png")
+
