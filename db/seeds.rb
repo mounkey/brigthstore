46771de9c8127a80b248e2991eedd87fec8b2aa8
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2021-09-22 17:42:46 -0300

    Fix general issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# User.create(first_name: "Slav", last_name: "Lewagon", email: "s@lewagon.com", password: "123edc")
-# Category.create(nombre: "Pantalones")
-# Category.create(nombre: "Remeras")
+User.create(email: "lfuentes.entergrafic@gmail.com", password: "15051996")
+User.create(email:"jpgrover@mounkey.cl", password: "12345678")
+User.create(email: "entergr4fic@gmail.com", password: "12345678")
+User.create(email: "megalatinvoz@gmail.com", password: "12345678")
 
 
-#User.create (email: "lfuentes.entergrafic@gmail.com", password: "15051996", admin: true)
-#User.create (email:"jpgrover@mounkey.cl", password: "12345678", admin: true)
-#User.create (email: "entergr4fic@gmail.com" password: "12345678", admin: false)
-#User.create (email: "megalatinvoz@gmail.com", password: "12345678", admin: false)
-
-#wear = Wear.create([{marca: 'fiorucci', color: 'rosado', talla: '42', descripcion: 'chaqueta mezclilla color rosa', valor: '20.000', category_id: '2', user_id: '1'}, {marca: 'zara', color: 'floreado', talla: 's', descripcion: 'vestido corto genero liviano', valor: '15.000', category_id: '1', user_id: '1'}, {marca: 'levis', color: 'denim', talla: '44', descripcion: 'jeans short gastado', talla: '14', category_id: '1', user_id: '1'}, {marca: 'oneil', color: 'azul', talla: 'm', descripcion: 'traje de baño piezas sin uso', category_id: '2', valor: '3000', category_id: '1', user_id: '1'}, {marca: 'werever', color: 'café', talla: 'xl', descripcion: 'falda café con botones y etc ejemplo largo', valor: '3000', category_id: '1', user_id: '1'}, {marca: 'infanti', color: 'rosado', talla: '3', descripcion: 'chaleco forrado niña', valor: '3000', category_id: '3', user_id: '1'}, {marca: 'casa_blanca', color: 'blanco', talla: '3', descrpcion: 'vestido fiesta niña', valor: '10000', category_id: '3', user_id: '1'}, {marca: 'mary', color: 'rojo', talla: '43', descrpcion: 'vestido fiesta lola', valor: '1', category_id: '1', user_id: '6'}, {marca: 'caballito', color: 'rojo', talla: '2', descripcion: 'tenida', valor: '30', category_id: '5', user_id: '1'}])
-
-
-#Category.create(nombre: "Ropa_Mujer", photo: "picturesWomanbig.png")
-#Category.create(nombre: "Ropa_Kids", photo: "picturesKidssmall.png")
-#Category.create(nombre: "Ropa_Hombre", photo: "picturesManbig.png")
-#Category.create(nombre: "Ropa_Winter", photo: "picturesWintersmall.png")
-#Category.create(nombre: "Ofertas", photo: "picturesSalesmall.png")
-#Category.create(nombre: "Party", photo:"picturesPartysmall.png")
-
+Category.create(nombre: "Ropa_Mujer", imagen: "picturesWomanbig.png")
+Category.create(nombre: "Ropa_Kids", imagen: "picturesKidssmall.png")
+Category.create(nombre: "Ropa_Hombre", imagen: "picturesManbig.png")
+Category.create(nombre: "Ropa_Winter", imagen: "picturesWintersmall.png")
+Category.create(nombre: "Ofertas", imagen: "picturesSalesmall.png")
+Category.create(nombre: "Party", imagen:"picturesPartysmall.png")
