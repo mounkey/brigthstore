a5ffb359c3a64ef6a2ffe37bba5cc865a5328521
juan grover <jpgrover@gmail.com>
2021-09-23 15:55:08 -0300

    Listo el Migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-User.create(email: "lfuentes.entergrafic@gmail.com", password: "15051996")
-User.create(email:"jpgrover@mounkey.cl", password: "12345678")
-User.create(email: "entergr4fic@gmail.com", password: "12345678")
-User.create(email: "megalatinvoz@gmail.com", password: "12345678")
+Category.destroy_all
+User.destroy_all
+Wear.destroy_all
+
+User.create(email: "lfuentes.entergrafic@gmail.com", password: "15051996", admin: true)
+User.create(email:"jpgrover@mounkey.cl", password: "12345678", admin: true)
+User.create(email: "entergr4fic@gmail.com", password: "12345678", admin: false)
+User.create(email: "megalatinvoz@gmail.com", password: "12345678", admin: false)
 
 
 Category.create(nombre: "Ropa_Mujer", imagen: "picturesWomanbig.png")
