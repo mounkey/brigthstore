2ac19ea2667c9bc348308f4d6f5ce4be4e871136
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-09-24 16:22:08 -0300

    search, navbar, cards, botones, show, categories y seed arreglado

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,10 @@ Category.destroy_all
 User.destroy_all
 Wear.destroy_all
 
-
-User.create(email: "lfuentes.entergrafic@gmail.com", password: "15051996")
-User.create(email:"jpgrover@mounkey.cl", password: "12345678")
-User.create(email: "entergr4fic@gmail.com", password: "12345678")
-User.create(email: "megalatinvoz@gmail.com", password: "12345678")
-
 User.create(email: "lfuentes.entergrafic@gmail.com", password: "15051996", admin: true)
 User.create(email:"jpgrover@mounkey.cl", password: "12345678", admin: true)
 User.create(email: "entergr4fic@gmail.com", password: "12345678", admin: false)
 User.create(email: "megalatinvoz@gmail.com", password: "12345678", admin: false)
-
-
-
 
 Category.create(nombre: "Ropa_Mujer", imagen: "picturesWomanbig.png")
 Category.create(nombre: "Ropa_Kids", imagen: "picturesKidssmall.png")
@@ -22,4 +13,3 @@ Category.create(nombre: "Ropa_Hombre", imagen: "picturesManbig.png")
 Category.create(nombre: "Ropa_Winter", imagen: "picturesWintersmall.png")
 Category.create(nombre: "Ofertas", imagen: "picturesSalesmall.png")
 Category.create(nombre: "Party", imagen:"picturesPartysmall.png")
-  
