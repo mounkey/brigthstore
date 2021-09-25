a85dde8a57752f24f9c55431ed8169bfd1f969b8
juan grover <jpgrover@gmail.com>
2021-09-25 17:22:46 -0300

    Listo algunos detalles

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:destroy, :show, :update, :edit]
+  before_action :set_order, only: [:destroy, :show, :update, :edit, :address]
 
   def index
     @orders = Order.all
@@ -40,6 +40,21 @@ class OrdersController < ApplicationController
     end
   end
 
+  def address
+    @search = params["address"]
+    if @search.present?
+      @adress = @search["address"]
+      @city =  @search["city"]
+      @state = @search["state"]
+      @country = @search["contry"]
+      @order[:adress] = @adress
+      @order[:city] = @city
+      @order[:state] = @state
+      @order[:country]= @country
+      redirect_to search_path
+    end
+  end
+
   private
 
   def set_order
@@ -50,6 +65,8 @@ class OrdersController < ApplicationController
     params.require(:order).permit(:monto, :fecha, :order_detail, :order_detail_id)
   end
 
+  def order_prams2
+    params.requiere(:order).permit(:address, :city, :state, :country  )
+  end
 
-  
 end
